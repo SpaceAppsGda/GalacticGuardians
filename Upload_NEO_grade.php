e6a882a14b1b029a639f1d06df25989cbf024f2f
Ferdinand <a01064282@itesm.mx>
2013-04-21 04:06:10 -0500

    asdf

--- Upload_NEO_grade.php
+++ Upload_NEO_grade.php
@@ -4,10 +4,10 @@ $Admin = mysql_connect("10.43.7.44" , "GG" , "starlord") or die (mysql_error());
 mysql_select_db("user");
 $tabla = mysql_query("SELECT * FROM UserName") or die (mysql_error());
 if (isset($_COOKIE["user"])){
-	mysql_query("INSERT INTO rating(UserName)
-	VALUES (
-	'{$_POST["user"]}')",
-	$Admin);
+	echo $_COOKIE["user"];
 }
 
+
+
+$Admin);
 ?>	
