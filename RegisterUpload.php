d4ebc0f50c99a2eeaaca26383edeba1ce8660e88
Jorge <Jorgeleon12@hotmail.com>
2013-04-20 21:02:02 -0500

    YA FUNCIONA!! (CREO!!)

--- RegisterUpload.php
+++ RegisterUpload.php
@@ -26,7 +26,7 @@ echo $Country."<br>";
 echo $Occupation."<br>";
 echo $Organization."<br>";
 
-if($Pass == $Pass2 && $Pass!= null)	//valida contraseña, se sean iguales
+if($Pass == $Pass2 && !is_null($Pass) && !is_null($FirstName) && !is_null($LastName) && !is_null($UserName) && !is_null($Email) && !is_null($Phone) && !is_null($Country) && !is_null($Occupation) && !is_null($Organization))	//valida contraseña, se sean iguales
 {
 	$Admin = mysql_connect("10.43.7.44" , "GG" , "starlord");
 	$bdNEO = mysql_select_db("neo",$Admin);
@@ -35,33 +35,45 @@ if($Pass == $Pass2 && $Pass!= null)	//valida contraseña, se sean iguales
 	while($row=mysql_fetch_array($tabla)){
 		if ($row[1] != $UserName and $row[4] != $Email)
 		{
-			echo"Checking Data: "."$row[1]"."  "."$row[4]"."<br>";
+//			echo"Checking Data: "."$row[1]"."  "."$row[4]"."<br>";
+			$Finaliza = "si";
 		}
 		else{
-			echo "Existing Data"."<br>";
+			function error(){
+				echo'Alert("Existing Data! try again with other Username/Mail")';
+			}
+			$Finaliza = "no";
 			break;
 		}
 	}
-	echo "Registration Successful!";
-	mysql_query("INSERT INTO user(UserName, FirstName, LastName, Email, Pass, Phone, Country, Occupation, Organization) 
-	VALUES (
-	'{$_POST["UserName"]}',
-	'{$_POST["FirstName"]}',
-	'{$_POST["LastName"]}',
-	'{$_POST["Email"]}',
-	'{$_POST["Pass"]}',
-	'{$_POST["Phone"]}',
-	'{$_POST["Country"]}',
-	'{$_POST["Occupation"]}',
-	'{$_POST["Organization"]}')",
-	$Admin);
 	
-	//header("Location:index.php");
+	if($Finaliza == "si"){
+		mysql_query("INSERT INTO user(UserName, FirstName, LastName, Email, Pass, Phone, Country, Occupation, Organization) 
+		VALUES (
+		'{$_POST["UserName"]}',
+		'{$_POST["FirstName"]}',
+		'{$_POST["LastName"]}',
+		'{$_POST["Email"]}',
+		'{$_POST["Pass"]}',
+		'{$_POST["Phone"]}',
+		'{$_POST["Country"]}',
+		'{$_POST["Occupation"]}',
+		'{$_POST["Organization"]}')",
+		$Admin);
+		header("Location:index.php");
+		function Error(){echo'Alert("Registration Successful!")';}
+	}
+	else{
+		function Error(){echo 'Alert("PassWord/Username it is on Use!")';}
+	}
+}
+else if($Pass != $Pass2){
 	
+	function Error(){echo 'Alert("Passwords did not match")';}
 }
 else{
-    echo "Passwords didn't match";
-    }
+	function Error(){echo 'Alert("Fill all the Spaces Pleas!")';}
+}
 ?>
 </body> 
 </html>
