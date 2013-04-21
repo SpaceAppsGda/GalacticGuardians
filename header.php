0a66ba394901d8998612d6c1f91c7417a5d20bec
Edna Ramirez <edna.jahel.r@gmail.com>
2013-04-20 20:06:48 -0500

    Agregado header.php, home.php, footer.php

---
+++ header.php
@@ -0,0 +1,23 @@
+<div class="navbar navbar-inverse navbar-fixed-top">
+  <div class="navbar-inner">
+    <div class="container-fluid">
+      <button type="button" class="btn btn-navbar" data-toggle="collapse" data-target=".nav-collapse">
+        <span class="icon-bar"></span>
+        <span class="icon-bar"></span>
+        <span class="icon-bar"></span>
+      </button>
+      <a class="brand" href="#">NEObserver</a>
+      <div class="nav-collapse collapse">
+         <form class="navbar-form pull-right">
+            <input class="span2" type="text" placeholder="Username">
+            <input class="span2" type="password" placeholder="Password">
+            <button type="submit" class="btn">Log In</button>
+            <button class="btn">Register</button>
+          </form>
+          <ul class="nav">
+              <li class="active"><a href="#">Add NEO</a></li>
+          </ul>
+      </div><!--/.nav-collapse -->
+    </div>
+  </div>
+</div>
---
+++ home.php
@@ -0,0 +1,39 @@
+<style type="text/css">
+      body {
+        padding-top: 20px;
+        padding-bottom: 60px;
+      }
+
+      /* Main marketing message and sign up button */
+      .jumbotron {
+        margin: 80px 0;
+        text-align: center;
+      }
+      .jumbotron h1 {
+        font-size: 100px;
+        line-height: 1;
+      }
+      .jumbotron .lead {
+        font-size: 24px;
+        line-height: 1.25;
+      }
+      .jumbotron .btn {
+        font-size: 21px;
+        padding: 14px 24px;
+      }
+
+      /* Supporting marketing content */
+      .marketing {
+        margin: 60px 0;
+      }
+      .marketing p + h4 {
+        margin-top: 28px;
+      }
+</style>
+<div class="row">
+<div class="jumbotron">
+  <h3>They are all around our planet!</h3>
+  <p class="lead">Near Earth Objects </p>
+  <a class="btn btn-large btn-success" href="#">Get started today</a>
+</div>
+</div>
