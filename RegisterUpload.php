d596cf9f2ed5f2ee8b7e8b195c533d0e3be53350
Iván Rodríguez López <ivan_i1@hotmail.com>
2013-04-20 22:05:08 -0500

    Esto esta loco

--- RegisterUpload.php
+++ RegisterUpload.php
@@ -15,6 +15,7 @@ $Phone = $_POST["Phone"];
 $Country = $_POST["Country"];
 $Occupation = $_POST["Occupation"];
 $Organization = $_POST["Organization"];
+
 echo $FirstName."<br>";
 echo $LastName."<br>";
 echo $UserName."<br>";
@@ -25,15 +26,8 @@ echo $Phone."<br>";
 echo $Country."<br>";
 echo $Occupation."<br>";
 echo $Organization."<br>";
-<<<<<<< HEAD
-$hola = "";
-$Hola2 = empty($hola);
-echo $Hola2;/*
-if($Pass == $Pass2 && (!empty($Pass)) && !is_null($FirstName) && !is_null($LastName) && !is_null($UserName) && !is_null($Email) && !is_null($Phone) && !is_null($Country) && !is_null($Occupation) && !is_null($Organization))	//valida contraseña, se sean iguales
-=======
 
 if($Pass == $Pass2 && !empty($Pass) && !empty($FirstName) && !empty($LastName) && !empty($UserName) && !empty($Email) && !empty($Phone) && !empty($Country) && !empty($Occupation) && !empty($Organization))	//valida contraseña, se sean iguales
->>>>>>> 8dbbdd90b699478e68ba72b16a79b32bcddb6abd
 {
 	$Admin = mysql_connect("10.43.7.44" , "GG" , "starlord");
 	$bdNEO = mysql_select_db("neo",$Admin);
@@ -56,11 +50,7 @@ if($Pass == $Pass2 && !empty($Pass) && !empty($FirstName) && !empty($LastName) &
 	}
 	
 	if($Finaliza == "si"){
-<<<<<<< HEAD
-/*		mysql_query("INSERT INTO user(UserName, FirstName, LastName, Email, Pass, Phone, Country, Occupation, Organization) 
-=======
 		/*mysql_query("INSERT INTO user(UserName, FirstName, LastName, Email, Pass, Phone, Country, Occupation, Organization) 
->>>>>>> 8dbbdd90b699478e68ba72b16a79b32bcddb6abd
 		VALUES (
 		'{$_POST["UserName"]}',
 		'{$_POST["FirstName"]}',
@@ -71,13 +61,8 @@ if($Pass == $Pass2 && !empty($Pass) && !empty($FirstName) && !empty($LastName) &
 		'{$_POST["Country"]}',
 		'{$_POST["Occupation"]}',
 		'{$_POST["Organization"]}')",
-<<<<<<< HEAD
-		$Admin);
-*		echo "<script>alert('Registration Successful!')</script>";
-=======
 		$Admin);*/
 		echo "<script>alert('Registration Successful!')</script>";
->>>>>>> 8dbbdd90b699478e68ba72b16a79b32bcddb6abd
 		header("Location:index.php");
 	}
 	else{
@@ -93,7 +78,6 @@ else{
 	echo "<script>alert('Fill all the blank spaces please')</script>";
 	echo"<script>history.go(-1)</script>";
 }
-*/
 ?>
 
 </body> 
